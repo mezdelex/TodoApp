1b447508e84f5de7f99c7fbe5d5db27f041ea3f1
Mezdelex <59997405+mezdelex@users.noreply.github.com>
2023-09-19 11:02:24 +0200

    (feature) Add CI (#19)
    * added CI workflow and integration test controll on CI flow
    
    * added CI to main branch on push and on PR

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	todosService "todoapp.com/application/services/todos"
 	"todoapp.com/infrastructure/connectors/postgre"
+	"todoapp.com/infrastructure/environments"
 	todosRepository "todoapp.com/infrastructure/repositories/todos"
 	todosController "todoapp.com/presentation/controllers/todos"
 )
 
 func main() {
+	error := environments.LoadEnv()
+	if error != nil {
+		log.Fatal("Error loading .env file.")
+	}
+
 	db := postgre.Connect()
 
 	app := fiber.New(fiber.Config{JSONEncoder: json.Marshal, JSONDecoder: json.Unmarshal})
